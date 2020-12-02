86ed7c4bced48187e7a55078e0ba1006e92377fb
Nicolas Audinet <nicolas.audinet@relexsolutions.com>
2020-12-02 10:32:24 +0200

    Use list comprehensions for day1

--- days/Day1.hs
+++ days/Day1.hs
@@ -1,24 +1,12 @@
-{-# LANGUAGE TupleSections #-}
 {-# LANGUAGE TypeApplications #-}
 
 module Main where
 
 import Paths_aoc2020
-import Data.List (subsequences)
-
-pairs :: [a] -> [(a,a)]
-pairs [] = []
-pairs (x:xs) = map (x,) xs <> pairs xs
-
-triples :: [a] -> [(a,a,a)]
-triples [] = []
-triples (x:xs) = map (\(a,b) -> (x,a,b)) (pairs xs) <> triples xs
 
 main :: IO ()
 main = do
   file <- getDataFileName "inputs/day1"
-  numbers <- map (read @Int) . lines <$> readFile file
-  let [(a,b)] = filter (\(a,b) -> a + b == 2020) (pairs numbers)
-  print (a * b)
-  let [(x,y,z)] = filter (\(x,y,z) -> x + y + z == 2020) (triples numbers)
-  print (x * y * z)
+  nums <- map (read @Int) . lines <$> readFile file
+  print $ head [ a * b | a <- nums, b <- nums, a + b == 2020 ]
+  print $ head [ a * b * c | a <- nums, b <- nums, c <- nums, a + b + c == 2020 ]
