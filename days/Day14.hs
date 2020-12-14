fbdd05b8b5ec2e709e88cc180013bf0b5e1223f1
Nicolas Audinet <nic.audinet27@gmail.com>
2020-12-15 00:48:05 +0200

    Day 14 correct solutions

--- days/Day14.hs
+++ days/Day14.hs
@@ -9,7 +9,7 @@ import Data.List
 import Data.List.Split
 import qualified Data.Bits.Bitwise as B
 import qualified Data.Map as M
-import qualified Data.Set as S
+import GHC.Exts (sortWith)
 
 data MaskBit = M1 | M0 | MX
   deriving Show
@@ -71,21 +71,21 @@ step (Mem (addr, num)) DecodeState{..} =
 run :: [Command] -> DecodeState
 run = foldl' (flip step) (DecodeState [] M.empty)
 
-memSum :: DecodeState -> Int
-memSum = sum . M.elems . mem
+memSum :: M.Map key Int -> Int
+memSum = sum . M.elems
 
 
 
 type Address = [Bool]
-type AddressMap = M.Map Int (S.Set Address)
+type AddressMap = M.Map Address Int
 data DecodeStateV2 = DecodeStateV2
   { maskV2 :: [MaskBit]
   , addrMap :: AddressMap
  }
   deriving Show
 
-space :: [MaskBit] -> Address -> S.Set Address
-space mask addr = S.fromList . go $ zip mask addr
+space :: [MaskBit] -> Address -> [Address]
+space mask addr = go $ zip mask addr
   where
     go :: [(MaskBit, Bool)] -> [Address]
     go [] = [[]]
@@ -99,14 +99,8 @@ stepV2 :: Command -> DecodeStateV2 -> DecodeStateV2
 stepV2 (Mask bits) (DecodeStateV2 _ addrMap) = DecodeStateV2 bits addrMap
 stepV2 (Mem (addr, value)) (DecodeStateV2 mask addrMap) =
   let addrSpace = space (reverse mask) (B.toListLE addr)
-      newAddrMap = M.insert value addrSpace (M.map (S.\\ addrSpace) addrMap)
+      newAddrMap = foldr (\k -> M.insert k value) addrMap addrSpace
   in DecodeStateV2 mask newAddrMap
-
-memSumV2 :: DecodeStateV2 -> Integer
-memSumV2
-  = M.foldrWithKey (\k v i -> (fromIntegral k) * (fromIntegral v) + i) 0
-  . M.map (fromIntegral . S.size)
-  . addrMap
 
 runV2 :: [Command] -> DecodeStateV2
 runV2 = foldl' (flip stepV2) (DecodeStateV2 [] M.empty)
@@ -119,34 +113,21 @@ test = parse . unlines $
   , "mem[26] = 1"
   ]
 
-test2 :: [Command]
-test2 = parse . unlines $
-  [ "mask = 1110X1110XXX101X0011010X110X10X0110X"
-  , "mem[40257] = 51331021"
-  ]
+showAddr :: [Bool] -> String
+showAddr = map (\x -> if x then '1' else '0')
 
-showAddrSpace :: S.Set [Bool] -> String
-showAddrSpace =
-  unlines . map (map (\x -> if x then '1' else '0')) . reverse . S.toList
+showKeyValue :: ([Bool], Int) -> String
+showKeyValue (k, v) = unwords [showAddr k, "->", show v]
 
-showKeyValue :: (Int, S.Set [Bool]) -> String
-showKeyValue (k, v) = unlines ["---", show k, showAddrSpace v]
-
-showMap :: M.Map Int (S.Set [Bool]) -> String
-showMap = unlines . map showKeyValue . M.toList
+showMap :: M.Map [Bool] Int -> String
+showMap = unlines . map showKeyValue . sortWith snd . M.toList
 
 main :: IO ()
 main = do
   commands <- fmap parse . readFile =<< getDataFileName "inputs/day14"
-  mapM_ print commands
-  print (memSum $ run commands)
-  putStrLn "---"
-  putStrLn . showAddrSpace $
-    space
-      (reverse [M0, MX, M1, M0, M0, M1, MX])
-      (reverse [False, True, False, True, False, True, False])
-  putStrLn "---"
+  putStrLn "--- Test ---"
   putStrLn (showMap . addrMap $ runV2 test)
-  print (memSumV2 $ runV2 test)
-  print (memSumV2 $ runV2 test2)
-  print (memSumV2 $ runV2 commands)
+  print (memSum . addrMap $ runV2 test)
+  putStrLn "--- Real ---"
+  print (memSum . mem $ run commands)
+  print (memSum . addrMap $ runV2 commands)
